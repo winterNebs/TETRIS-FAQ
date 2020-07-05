5fd83c0fc4159e2e2488522bedbff0912dd95c6d
winterNebs <isaac@wen.ca>
2020-07-05 12:09:13 -0400

    minor additions

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
     someSidebar: {
-        "Tetris FAQ": ['intro', 'general', 'sprint', 'versus', 'tspin','hd'],
+        "Tetris FAQ": ['intro', 'general', 'sprint', 'versus', 'tspin','cheese','hd'],
         About: ['about']
     },
 };
